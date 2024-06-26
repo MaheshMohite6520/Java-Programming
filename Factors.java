16c5fcc642ac17f6e78a3eadf70b6762fdac70f4
Mahesh Mohite <maheshmohite6520@gmail.com>
2024-06-26 20:31:41 +0530

    few changes made

--- Factors.java
+++ Factors.java
@@ -8,16 +8,26 @@ public class Factors {
     System.out.print("Enter a Number: ");
     int num = sc.nextInt();
 
+    // 1 and Number itself are factors also
+    int count = 2;
+
     System.out.print("Factors of " + num + ": ");
 
-    System.out.print(1 + ", ");
+    System.out.print(1 + ", ");   // Common Factor is 1 for all numbers
 
     for(int i = 2; i <= num/2; i++)
     {
       if(num % i == 0)
       {
         System.out.print(i + ", ");
+        count++;
       }
     }
+
+    System.out.print(num);    // Number itself
+
+    System.out.println();
+
+    System.out.print("Count of Factors: " + count);
   }
 }
